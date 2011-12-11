1f125e6a6ee2d083e541a269d57247085c57fe79
Baldur Þór Emilsson <baldur@baldur.biz>
2011-12-11 16:22:51 +0000

    Added version 0.2.0

--- skutli/script.js
+++ skutli/script.js
@@ -2,6 +2,16 @@ $(function() {
     $(".screenshot-link").fancybox({
         openEffect: 'none',
         closeEffect: 'none',
+        nextEffect: 'fade',
+        prevEffect: 'fade',
+        loop: false,
         helpers: {}
+    });
+    
+    $("#old-versions-link").click(function () {
+        $(this).hide();
+        $("#old-versions").slideDown();
+        
+        return false;
     });
 });
